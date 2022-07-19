946c362bc86bb130abbff0a3cc5ce2c9edd851e5
bryanDev743 <bryanDev743@gmail.com>
2022-07-19 16:56:26 -0500

    gui opne works

--- imgTest.py
+++ imgTest.py
@@ -1,13 +1,29 @@
 import os
 from os import listdir
-
 from tkinter import filedialog
+from distutils.cmd import Command
+from tkinter import *
+from PIL import ImageTk, Image
+
 
 # get the path/directory 
 folder_dir = filedialog.askdirectory()
+
+gui = Tk()
+gui.title('imgTest')
+gui.geometry('500x300')
+
+
 for images in os.listdir(folder_dir):
 
 	# check if the image ends with png
 	if (images.endswith(".png")):
-		print(images, " type : ", type(images) )
-        
+          images = os.path.join(folder_dir,images)
+          i =  Image.open(images)       
+          i.show()
+         #print(images)
+         
+
+
+
+gui.mainloop()
