d71d9584f980dfb7c349b57b7214604a0bfa45f4
bryanDev743 <bryanDev743@gmail.com>
2022-07-19 16:06:47 -0500

    gui + reszing

--- gui.py
+++ gui.py
@@ -1,4 +1,29 @@
-import tkinter
-top = tkinter.Tk()
+from distutils.cmd import Command
+from tkinter import *
+from PIL import ImageTk, Image
+
+top = Tk()
+
+top.title('Gui')
+top.geometry('400x400')
+
+frame = Frame(top, width=50,height=50)
+frame.pack()
+frame.place(anchor='center',relx=0.5, rely=0.5)
+
+i = Image.open('resources/fire.png')
+i.thumbnail((100,100))
+img = ImageTk.PhotoImage(i)
+
+
+Label(top, image=img).pack()
+
+exitB = Button(top,text='exit',command=lambda:top.destroy())
+exitB.pack(pady=20)
 
 top.mainloop()
+
+# to do later
+# image = Image.open('demo_image.jpg')
+# image.thumbnail((400, 400))
+# image.save('image_thumbnail.jpg')
---
+++ img.py
@@ -0,0 +1,8 @@
+import resource
+from PIL import Image
+
+img = Image.open('resources/fire.png')
+
+print(img.size)
+
+#img.show()
